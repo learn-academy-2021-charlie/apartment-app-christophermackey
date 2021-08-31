386c30df38d5f2ce2c12538fdb2176de52d55eaa
christophermackey <c.mackey@me.com>
2021-08-31 09:53:39 -0700

    adds devise, rspec, seeds, and apartment model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+    {
+        email:'chris@test.com'
+        password: 'test123'
+        password_confirmation: 'test123'
+    }
+]
+
+users.each do |attribute|
+    User.create attribute
+end
+
+apartments = [
+  {
+    street: '123 Side Street',
+    city: 'SD',
+    state: 'CA',
+    manager: 'Sam',
+    email: 'sam@test.com',
+    price: '1400',
+    bedrooms: 1,
+    bathrooms: 1,
+    pets: '3 pets max'
+  },
+  {
+    street: '683 Front Street',
+    city: 'SD',
+    state: 'CA',
+    manager: 'Bill',
+    email: 'bill@test.com',
+    price: '1900',
+    bedrooms: 2,
+    bathrooms: 3,
+    pets: 'gophers only'
+  }
+]
+
+first_user = User.where(email: 'chris@test.com').first
+
+apartments.each do |attribute|
+  first_user.apartments.create attribute
+end
